5d052a3ff4a343dee14277fe1d46ac41f3657811
rafael <rafaelmdeveloper@gmail.com>
2022-02-17 22:29:41 -0300

    18. Customizing the scrollbar and keeping the side and header fixed on scroll

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -6,4 +6,21 @@ export const Container = styled.div`
     background-color: ${props => props.theme.colors.primary};
 
     padding: 25px; //Espassamento de 25px em todos os lados do conteiner;
+
+    height: calc(100vh - 70px); //view port height, ou altura da tela;
+    overflow-y: scroll; //todo o conteudo que não colber, ele gera uma barra de rolagem;
+
+    ::-webkit-scrollbar{
+        width: 10px; //largura;
+    }
+
+    ::-webkit-scrollbar-thumb{  //botão de rolagem
+        background-color: ${props => props.theme.colors.secundary};
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-track{  //é a barra maior, que fica por fora
+        background-color: ${props => props.theme.colors.tertiary};
+        border-radius: 10px;
+    }
 `;
