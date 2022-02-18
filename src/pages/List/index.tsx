4e261a34904cd193b09c8b3fc1b90f7c74fefea1
rafael <rafaelmdeveloper@gmail.com>
2022-02-17 22:59:35 -0300

    20. Creating the filters

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,19 +4,42 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
-import{ Container, Content } from './styles';
+import{ Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
-    const options = [
-        {value: 'Rafael', label: 'Rafael'},
-        {value: 'Ana', label: 'Ana'},
-        {value: 'Fulano', label: 'Fulano'},
+    const months = [
+        {value: 7, label: 'Julho'},
+        {value: 8, label: 'Agosto'},
+        {value: 9, label: 'Setembro'},
+    ]
+    const years = [
+        {value: 2020, label: 2020},
+        {value: 2019, label: 2019},
+        {value: 2018, label: 2018},
     ]
     return (
         <Container>
             <ContentHeader title="Saídas" lineColor="#E44C4E" cjhildren={undefined}>
-               <SelectInput options={options}/> 
+               <SelectInput options={months}/> 
+               <SelectInput options={years}/> 
             </ContentHeader>
+
+            <Filters>
+                <button 
+                type="button"
+                className="tag-filter tag-filter-recurrent"
+                >
+                    Recorrentes
+                </button>
+                <button 
+                type="button"
+                className="tag-filter tag-filter-eventual"
+                >
+                    Eventuais
+                </button>
+            </Filters>
+
+
             <Content>
                 <HistoryFinanceCard
                     tagColor="#E44C4E"
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -4,3 +4,39 @@ export const Container = styled.div`
 `;
 export const Content = styled.div`
 `;
+export const Filters = styled.div`
+    width: 100%; //irá ocupar 100% da largura da tela;
+    display: flex;
+    justify-content: center; //para ficar tudo no centro;
+    margin-bottom: 30px; //para espaçar  card para baixo;
+
+    .tag-filter{
+        font-size: 18px; 
+        font-weight: 500; 
+        background: none; //para sumir o fundo;
+        color: ${props => props.theme.colors.white};
+
+        margin: 0 10px; //para ele se afatar um do outro;
+
+        transition: opacity -3s;
+
+        :hover{
+            opacity: .7;
+        }
+    }
+
+        .tag-filter-recurrent::after {
+            content: '';
+            display: block;
+            width: 55px;
+            margin: 0 auto;
+            border-bottom: 10px solid ${props => props.theme.colors.warning};
+    }
+        .tag-filter-eventual::after {
+            content: '';
+            display: block;
+            width: 55px;
+            margin: 0 auto;
+            border-bottom: 10px solid ${props => props.theme.colors.sucess};
+    }
+`;
